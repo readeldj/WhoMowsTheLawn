b4cdf124a6e77e05c574aec850a681ab2d5d24a6
Devin Readel <readeldj@gmail.com>
2015-03-26 22:37:52 -0500

    added $.get to repop data onto profile. cleaned and commented on profile.js. cleaned up login.html to hide more buttons

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,31 +1,37 @@
 'use strict';
 
+
+// setting global variables
 var FIREBASE_URL = 'https://whomowsthelawn.firebaseio.com',
               fb = new Firebase(FIREBASE_URL),
-           $form = $('form')
-           token,
+           $form = $('form'),
       usersFbUrl;
 
-
+//this runs on loading of the js file.
 if (fb.getAuth()) {
   // $('.login').remove();
   // $('.app').toggleClass('hidden');
   // $('.test1').toggleClass('hidden');
   // $('.test2').toggleClass('hidden');
 
-  usersFbUrl = FIREBASE_URL + '/users/' + fb.getAuth().uid; //deleted  + '/data'
-  token = fb.getAuth().token;
+  usersFbUrl = FIREBASE_URL + '/users/' + fb.getAuth().uid;
 
-  $.get(usersFbUrl + '/users/' + fb.getAuth().uid + '/profile.json', function(data){
-    if(data !== null) {
-      Object.keys(data).forEach(function(uuid) {
-        //addProfileToTable(uuid, data[uuid]);
-        //showPetDiv();
-      });
-    }
+//this is where we get our data from fb and then add it to the DOM.
+//works but the submit button holds grey font color. fix
+  $.get(usersFbUrl + '.json', function(data){
+    $('input[id="address"]').val( data.address );
+    $('input[id="city"]').val(data.city);
+    $('select[id="state"]').val(data.state);
+    $('input[id="zip"]').val(data.zip);
+    $('input[id="price"]').val(data.price);
+    $('input[id="website"]').val(data.website);
+    $('input[id="facebook"]').val(data.facebook);
+    $('select[id="rating"]').val(data.rating);
+    $('textarea[id="comments"]').val(data.comments);
   });
 }
 
+//This is where we submit our profile form to fb.
 $form.submit(function(evt){
    var $address   = $('input[id="address"]').val(),
        $city      = $('input[id="city"]').val(),
@@ -46,18 +52,11 @@ $form.submit(function(evt){
                       facebook: $facebook,
                       rating: $rating,
                       comments: $comments };
-
+//I want to redo this so that it uses jquery/ajax so it's not fb specific so I can move it somewhere else later.
    var ref = new Firebase("https://whomowsthelawn.firebaseio.com/users/");
    var usersRef = ref.child(fb.getAuth().uid);
    usersRef.set(group);
 
 
-
-   //var url  = usersFbUrl + '/profile.json'; //was usersFbUrl + '/profile.json?auth=' + token;
-   //var data = JSON.stringify(group);
-   //$.post(url, data, function(res){
-    //  $tr.attr('data-uuid', res.name);
-    //  $tbody.append($tr);
-   //});
    evt.preventDefault();
  });
