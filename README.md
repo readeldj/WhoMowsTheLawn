36f94f4d4d3bbbb84cb8277bfb97540a51a29bcf
Devin Readel <readeldj@gmail.com>
2015-04-06 11:49:14 -0500

    a little cleanup before presentation

--- README.md
+++ README.md
@@ -2,3 +2,18 @@
 Map out lawn service pricing by end users.
 
 Start of the project for end of class.
+
+User can create Login and setup profile.
+User adds:
+  Address for mapping.
+  Lawn Provider Website and/or Facebook page.
+  Price for service.
+  Comments.
+
+No login required to search mapped data.
+
+
+Bower Dependencies:
+  skeleton: 2.0.4
+  jquery: 2.1.3
+  firebase: 2.2.3
--- public/js/main.js
+++ public/js/main.js
@@ -2,23 +2,56 @@
 //initialize();
 
 function initialize() {
-  var myLatlng = new google.maps.LatLng(36.124, -86.725);  //Schools location for testing.
+  var myLatlng = new google.maps.LatLng(31.569177, -91.3473639999999);  //Schools location for testing.
   //var myLatlng = new google.maps.LatLng([app:user-lat], [app:user-lon]);
   var mapOptions = {
-    zoom: 14,
+    zoom: 15,
     center: myLatlng
   }
   var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
+  // var marker = new google.maps.Marker({
+  //     position: myLatlng,
+  //     map: map,
+  //     title: 'Hello World!',
+  //     animation: google.maps.Animation.DROP,
+  //     labelContent: "$425K",
+  //     labelAnchor: new google.maps.Point(22, 0),
+  //     labelClass: "labels" // the CSS class for the label
+  // });
+
+  var contentString = '<div id="content">'+
+      // '<div id="siteNotice">'+
+      // '</div>'+
+      //'<h1 id="firstHeading" class="firstHeading">400 Oakland Dr</h1>'+
+      '<div id="bodyContent">'+
+      '<p>400 Oakland Dr<br>' +
+      'Natchez, MS 39120<br>'+
+      'Website: <a href="http://uslawns.com/ken-beasley-system-success/">uslawns.com</a><br>'+
+      'Facebook: <a href="https://www.facebook.com/uslawnsfranchise">www.facebook.com/uslawnsfranchise</a><br>'+
+      'Price: $30<br>'+
+      'Comments: Grass is even, edging is perfect, no mess left behind.<br>'+
+      '</p>'+
+      '</div>'+
+      '</div>';
+
+  var infowindow = new google.maps.InfoWindow({
+      content: contentString,
+      maxWidth: 200
+  });
+
   var marker = new google.maps.Marker({
       position: myLatlng,
       map: map,
-      title: 'Hello World!',
-      animation: google.maps.Animation.DROP,
-      labelContent: "$425K",
-      labelAnchor: new google.maps.Point(22, 0),
-      labelClass: "labels" // the CSS class for the label
+      title: '400 Oakland Dr'
   });
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.open(map,marker);
+  });
+
+
+
+
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
